cd0b19f5a1126038b130ef4e497e1bb0004a1ee7
Chia Carlyle <chiacarlyle@gmail.com>
2022-03-02 22:07:42 +0100

    complete the test

--- js/stringLength.js
+++ js/stringLength.js
@@ -1,3 +1,9 @@
-const stringLength = string => string.length;
+const stringLength = string => {
+  if (string.length < 1 || string.length > 10) {
+    return "error";
+  } else {
+    return string.length;
+  }
+}
 
 module.exports = stringLength
--- js/stringLength.test.js
+++ js/stringLength.test.js
@@ -5,3 +5,11 @@ const stringLength = require('./stringLength')
 it('count string characters', () => {
   expect(stringLength('string')).toBe(6)
 })
+
+it('restrict number of string characters', () => {
+  expect(stringLength('i hate you...')).toBe("error")
+})
+
+it('restrict number of string characters2', () => {
+  expect(stringLength('')).toBe("error")
+})
